3441517316917c5a04fb040a9d680c190287b3f3
kennedy <kenneyrit@gmail.com>
2023-01-23 09:16:48 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,44 @@
 import React from "react";
-
-function QuestionItem({ question }) {
+function QuestionItem({ question, API, questions, setQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
-
   const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
+    <option key = {index}  value = {index}>
+    {answer}
     </option>
-  ));
+  )); 
+
+
+  function deleteQuestion(id) {
+    fetch(`${API}/${id}`, {
+      method: 'DELETE', 
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`deleted question with id: ${id}`)
+      const updatedQuestions = 
+      questions.filter((question) => question.id!==id)
+      setQuestions(() => updatedQuestions); 
+    })
+  }
+  function changeAnswer(id, correctIndex) {
+    fetch(`${API}/${id}`, {
+      method: 'PATCH', 
+      headers: {
+        'Content-Type': 'application/json', 
+      }, 
+      body: JSON.stringify({'correctIndex': correctIndex})
+    })
+    .then((response) => response.json())
+    .then(data => console.log(data))
+    const updatedQuestions = questions.map(question => {
+      if (question.id === id ) {
+        return {...question, correctIndex}
+      }
+      else {
+        return question
+      }
+    })
+  }
 
   return (
     <li>
@@ -15,9 +46,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange= {(event) => {
+          changeAnswer(id, event.target.value); 
+        }} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick ={() => deleteQuestion(id)} >Delete Question</button>
     </li>
   );
 }
